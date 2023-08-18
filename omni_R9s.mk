ee937986e20dcfe39db0a3a4529e4468bac645ca
Ngtc233 <89304330+Ngtc233@users.noreply.github.com>
2023-08-18 14:56:44 +0800

    Update omni_R9s.mk

--- omni_R9s.mk
+++ omni_R9s.mk
@@ -22,9 +22,6 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from R9s device
-$(call inherit-product, device/oppo/R9s/device.mk)
-
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
