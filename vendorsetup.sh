3b2d7ff8b97b1b389ce32743531e87eb955490f6
Ngtc233 <89304330+Ngtc233@users.noreply.github.com>
2024-07-29 16:19:16 +0800

    Add some features

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,7 +19,18 @@ export OF_FORCE_PREBUILT_KERNEL=1
 export OF_FL_PATH1=/sys/class/leds/led:torch_0
 export OF_FL_PATH2=/sys/class/leds/led:torch_1
 export OF_MAINTAINER=Ngtc233
-export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+export FOX_VANILLA_BUILD=1
+export OF_DISABLE_OTA_MENU=1
+export FOX_ENABLE_APP_MANAGER=1
+export FOX_USE_NANO_EDITOR=1
+export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+export OF_USE_SYSTEM_FINGERPRINT="1"
+export FOX_USE_BASH_SHELL=1
+export FOX_ASH_IS_BASH=1
+
 
 add_lunch_combo omni_R9s-userdebug
 add_lunch_combo omni_R9s-eng
